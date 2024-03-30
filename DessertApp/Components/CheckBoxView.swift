15fe09bcb39025bfb8866242b822acb8e0a0d9c8
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2024-03-30 18:53:45 -0300

    refactor CheckBoxView

--- DessertApp/Components/CheckBoxView.swift
+++ DessertApp/Components/CheckBoxView.swift
@@ -1,30 +1,23 @@
 import SwiftUI
 
 struct CheckBoxView: View {
+    enum Constants {
+        static let width: CGFloat = 30.0
+        static let lineWidth: CGFloat = 0.5
+        static let leading: CGFloat = 15
+    }
     
-    @State var checked = false
     let fill = Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
+    @State var checked = false
     
     var body: some View {
         Circle()
-            .stroke(style: StrokeStyle(lineWidth: 0.5))
+            .stroke(style: StrokeStyle(lineWidth: Constants.lineWidth))
+            .background(Circle().foregroundColor( checked ? fill : .clear))
             .foregroundStyle(.tertiary)
-            .frame(width: 30, height: 30, alignment: .center)
-            .padding(.leading, 15)
-            .onTapGesture {
-                checked.toggle()
-            }
-            .overlay {
-                Circle()
-                    .foregroundColor(checked ? fill : .clear)
-                    .foregroundStyle(.tertiary)
-                    .frame(width: 30, height: 30, alignment: .center)
-                    .padding(.leading, 15)
-                    .onTapGesture {
-                        checked.toggle()
-                    }
-                
-            }
+            .frame(width: Constants.width, height: Constants.width, alignment: .center)
+            .padding(.leading, Constants.leading)
+            .onTapGesture { checked.toggle() }
     }
     
 }
