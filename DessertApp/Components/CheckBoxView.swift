763d9943f5bc928b2a644f14c51e4d1bec2b690d
Cristiano Salla Lunardi <cristianosalla@gmail.com>
2024-03-30 20:12:15 -0300

    Refactor CheckBoxView

--- DessertApp/Components/CheckBoxView.swift
+++ DessertApp/Components/CheckBoxView.swift
@@ -5,6 +5,7 @@ struct CheckBoxView: View {
         static let width: CGFloat = 30.0
         static let lineWidth: CGFloat = 0.5
         static let leading: CGFloat = 15
+        static let circleOpacity: CGFloat = 0.1
     }
     
     let fill = Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
@@ -13,7 +14,7 @@ struct CheckBoxView: View {
     var body: some View {
         Circle()
             .stroke(style: StrokeStyle(lineWidth: Constants.lineWidth))
-            .background(Circle().foregroundColor( checked ? fill : .clear))
+            .background(Circle().foregroundColor( checked ? fill : .white.opacity(Constants.circleOpacity)))
             .foregroundStyle(.tertiary)
             .frame(width: Constants.width, height: Constants.width, alignment: .center)
             .padding(.leading, Constants.leading)
